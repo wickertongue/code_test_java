da9b3c5af5e0437eed91940bf79c49c5b9adf320
hayden <haydenwb@gmail.com>
2019-11-18 21:16:27 +0000

    initial commit, starting from generated pom

---
+++ src/main/java/com/codetest/experiment/code_test_java/CodeTestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codetest.experiment.code_test_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeTestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codetest/experiment/code_test_java/CodeTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetest.experiment.code_test_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
