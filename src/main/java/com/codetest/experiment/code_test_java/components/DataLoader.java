6bdfbb076751888e1813e280a242c4fce301eb86
hayden <haydenwb@gmail.com>
2019-11-18 21:45:11 +0000

    created fake data in dataloader, need to create repositories before it will function

--- src/main/java/com/codetest/experiment/code_test_java/components/DataLoader.java
+++ src/main/java/com/codetest/experiment/code_test_java/components/DataLoader.java
@@ -1,10 +1,30 @@
 package com.codetest.experiment.code_test_java.components;
 
+import com.codetest.experiment.code_test_java.models.Product;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements ApplicationRunner {
 
+
+
     public DataLoader() {
+    }
+
+    public void run(ApplicationArguments args) {
+
+        // products
+
+        Product heart = new Product("Lavendar Heart", 925);
+        // save
+        Product cufflinks = new Product("Personalised Cufflinks", 4500);
+        // save
+        Product shirt = new Product("Kids T-shirt", 1995);
+        // save
+
+
     }
 
 }
